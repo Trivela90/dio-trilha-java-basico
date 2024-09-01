903e306c92c6b8aacd4fc687d88c0023848c9af3
Henrique Trivelato <102193625+Trivela90@users.noreply.github.com>
2024-08-31 21:11:28 -0300

    Lógica das classes ContaTerminal (princiapal) e Conta Bancaria

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,80 @@
+import java.io.*;
+import java.util.Scanner; // biblioteca de input
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Muito mais eficiente que o Scanner
+        BufferedReader input = new BufferedReader( new InputStreamReader(System.in) ); // input eh objeto para controle de Entrada
+
+        int num;
+        String agenc;
+        String nome;
+        double sld;
+
+        System.out.println("Infome o numero da conta: ");
+        num = Integer.parseInt( input.readLine() );
+
+        System.out.println("Infome o numero da agencia: ");
+        agenc = input.readLine();
+
+        System.out.println("Infome o nome do cliente: ");
+        nome = input.readLine();
+
+        System.out.println("Infome o saldo da conta: ");
+        sld = Double.parseDouble( input.readLine() );
+
+        // Criando a conta
+        ContaBancaria conta1 = new ContaBancaria(num, agenc, nome, sld);
+
+        input.close();
+
     }
+}
+
+
+class ContaBancaria {
+
+    private int _numero;
+    private String _agencia;
+    private String _nome_cliente;
+    private double _saldo;
+
+    // Contrutor da classe ContaBancaria
+    ContaBancaria(int num, String agency, String name, double balance) {
+        _numero = num;
+        _agencia = agency;
+        _nome_cliente = name;
+        _saldo = balance;
+
+        mensagemCriacao();
+    }
+
+    // --- Metodos
+
+    public void mensagemCriacao() {
+
+        System.out.println("");
+        System.out.println("Olá " + getNomeCliente() + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                            + getAgencia() + ", conta " + getNumero() + " e seu saldo " 
+                            + getSaldo() + " já está disponível para saque.\n");
+    }
+    
+    public int getNumero() {
+        return _numero;
+    }
+
+    public String getAgencia() {
+        return _agencia;
+    }
+
+    public String getNomeCliente() {
+        return _nome_cliente;
+    }
+
+    public double getSaldo() {
+        return _saldo;
+    }
+
 }
 
